14b0e230a6055f7956a23b6c8a22bf44967045d0
MeyarAeini <meyar@Bahareh.>
2025-02-05 21:49:12 -0500

    initilization

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          hi there, this is meyar! i just added docker compose, this time i am going to learn you guys up, i am talking about you docker, 
+          kubernates and react and next js
+          \n do you hear me?
+
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
